c4735b6e6d427c32845a733d1b0f25e8b0cc34fb
Sergii Fasolko <Sergii_Fasolko@epam.com>
2019-01-23 14:05:01 +0200

    added FactoryMethod pattern

---
+++ Creational/FactoryMethod/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public class Point
+    {
+        // factory method
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        // factory method
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Point.NewCartesianPoint(0.5d, 0.3d));
+            Console.WriteLine();
+
+            Console.WriteLine(Point.NewPolarPoint(0.5d, 0.3d));
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
