eb8d84cdcda59e7a6eafb6a5fd2588bcae535cef
Sergii Fasolko <Sergii_Fasolko@epam.com>
2019-01-23 15:56:26 +0200

    added factory exercise

---
+++ Creational/Factory/Exercise.cs
@@ -0,0 +1,26 @@
+﻿namespace Factory
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    public class PersonFactory
+    {
+        private static int Index = 0;
+
+        public Person CreatePerson(string name)
+        {
+            var person = new Person(Index, name);
+            Index++;
+            return person;
+        }
+    }
+}
