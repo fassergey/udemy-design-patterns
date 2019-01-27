45b792fd72bbf369c9d2634808b202e697939769
Sergey Fasolko <fas.sergey@gmail.com>
2019-01-27 13:49:30 +0200

    added Bridge pattern

---
+++ Structural/Bridge/Exercise.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Bridge
+{
+    public interface IRenderer1
+    {
+        string WhatToRenderAs { get; }
+    }
+
+    public abstract class Shape1
+    {
+        public string Name { get; set; }
+
+        protected IRenderer1 renderer;
+
+        protected Shape1(IRenderer1 renderer)
+        {
+            this.renderer = renderer ?? throw new ArgumentNullException(paramName: $"{nameof(renderer)}");
+        }
+
+        public override string ToString()
+        {
+            return $"Drawing {Name} as {renderer.WhatToRenderAs}";
+        }
+    }
+
+    public class VectorRenderer1 : IRenderer1
+    {
+        public string WhatToRenderAs => "lines";
+    }
+
+    public class RasterRenderer1 : IRenderer1
+    {
+        public string WhatToRenderAs => "pixels";
+    }
+
+    public class Triangle : Shape1
+    {
+        public Triangle(IRenderer1 renderer) : base(renderer) => Name = "Triangle";
+    }
+
+    public class Square : Shape1
+    {
+        public Square(IRenderer1 renderer) : base(renderer) => Name = "Square";
+    }
+
+    //public class VectorSquare : Square
+    //{
+    //    public VectorSquare(IRenderer1 renderer) : base(renderer)
+    //    {
+    //    }
+
+    //    public override string ToString() => $"Drawing {Name} as lines";
+    //}
+
+    //public class RasterSquare : Square
+    //{
+    //    public RasterSquare(IRenderer1 renderer) : base(renderer)
+    //    {
+    //    }
+
+    //    public override string ToString() => $"Drawing {Name} as pixels";
+    //}
+
+    //public class VectorTriangle : Triangle
+    //{
+    //    public VectorTriangle(IRenderer1 renderer) : base(renderer)
+    //    {
+    //    }
+
+    //    public override string ToString() => $"Drawing {Name} as lines";
+    //}
+
+    //public class RasterTriangle : Triangle
+    //{
+    //    public RasterTriangle(IRenderer1 renderer) : base(renderer)
+    //    {
+    //    }
+
+    //    public override string ToString() => $"Drawing {Name} as pixels";
+    //}
+}
---
+++ Structural/Bridge/Program.cs
@@ -0,0 +1,93 @@
+﻿using Autofac;
+using System;
+using static System.Console;
+
+/* Connecting components together through abstractions */
+
+namespace Bridge
+{
+    public interface IRenderer
+    {
+        void RenderCircle(float radius);
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing a circle of radius {radius}");
+        }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing pixels for circle with radius {radius}");
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+
+        protected Shape(IRenderer renderer)
+        {
+            this.renderer = renderer ?? throw new ArgumentNullException(paramName: $"{nameof(renderer)}");
+        }
+
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+
+    public class Circle : Shape
+    {
+        private float radius;
+
+        public Circle(IRenderer rendered, float radius) : base(rendered)
+        {
+            this.radius = radius;
+        }
+
+        public override void Draw()
+        {
+            renderer.RenderCircle(radius);
+        }
+
+        public override void Resize(float factor)
+        {
+            radius *= factor;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IRenderer renderer = new RasterRenderer();
+            var renderer = new VectorRenderer();
+            var circle = new Circle(renderer, 5);
+
+            circle.Draw();
+            circle.Resize(2);
+            circle.Draw();
+
+
+
+            var cb = new ContainerBuilder();
+            cb.RegisterType<VectorRenderer>().As<IRenderer>().SingleInstance();
+            cb.Register((c, p) => new Circle(c.Resolve<IRenderer>(),
+                p.Positional<float>(0)));
+
+            using (var c = cb.Build())
+            {
+                var circle1 = c.Resolve<Circle>(new PositionalParameter(0, 0.5f));
+                circle1.Draw();
+                circle1.Resize(2);
+                circle1.Draw();
+            }
+
+            WriteLine(new Triangle(new RasterRenderer1()).ToString());
+        }
+    }
+}
