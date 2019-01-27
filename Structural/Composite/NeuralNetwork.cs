a17846d45ed942dd1b73f7472aaec9852cf1b38f
Sergey Fasolko <fas.sergey@gmail.com>
2019-01-27 15:40:55 +0200

    added Composite pattern

---
+++ Structural/Composite/NeuralNetwork.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Composite
+{
+    public static class ExtentionMethods
+    {
+        public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if (ReferenceEquals(self, other)) return;
+
+            foreach (var from in self)
+                foreach (var to in other)
+                {
+                    from.Out.Add(to);
+                    to.In.Add(from);
+                }
+        }
+    }
+
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float value;
+        public List<Neuron> In, Out;
+
+        public Neuron()
+        {
+            In = new List<Neuron>();
+            Out = new List<Neuron>();
+        }
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class NeuronLayer : Collection<Neuron>
+    {
+
+    }
+}
