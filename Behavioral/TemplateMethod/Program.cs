3ba760e6e6c90026c51ba3949a4ff4c0de4a9dc7
Sergey Fasolko <fas.sergey@gmail.com>
2019-02-17 11:13:16 +0200

    added TemplateMethod pattern

---
+++ Behavioral/TemplateMethod/Program.cs
@@ -0,0 +1,70 @@
+﻿using static System.Console;
+
+namespace TemplateMethod
+{
+    /* A high-level blueprint for an algorithm to be completed by inheritors. */
+
+    /*
+     * Allows us to define the 'skeleton' of the algorithm, with concrete
+     * implementations defined in subclasses.
+     */
+
+    public abstract class Game
+    {
+        public void Run()
+        {
+            Start();
+
+            while (!HaveWinner)
+            {
+                TakeTurn();
+            }
+
+            WriteLine($"Player {WinningPlayer} wins.");
+        }
+
+        protected int currentPlayer;
+        protected readonly int numberOfPlayers;
+
+        protected Game(int numberOfPlayers)
+        {
+            this.numberOfPlayers = numberOfPlayers;
+        }
+
+        protected abstract void Start();
+        protected abstract void TakeTurn();
+        protected abstract bool HaveWinner { get; }
+        protected abstract int WinningPlayer { get; }
+    }
+
+    public class Chess : Game
+    {
+        public Chess(int numberOfPlayers) : base(2)
+        {
+        }
+
+        protected override void Start()
+        {
+            WriteLine($"Starting a game of chess with {numberOfPlayers} players.");
+        }
+
+        protected override void TakeTurn()
+        {
+            WriteLine($"Turn {turn++} taken by player {currentPlayer}.");
+            currentPlayer = (currentPlayer + 1) % numberOfPlayers;
+        }
+
+        protected override bool HaveWinner => turn == maxTurns;
+        protected override int WinningPlayer => currentPlayer;
+
+        private int turn = 1;
+        private int maxTurns = 10;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Behavioral/TemplateMethod/UnitTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace TemplateMethod
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void ImpasseTest()
+        {
+            var c1 = new Creature(1, 2);
+            var c2 = new Creature(1, 2);
+            CardGame game = new TemporaryCardDamageGame(new[] { c1, c2 });
+            Assert.That(game.Combat(0, 1), Is.EqualTo(-1));
+            Assert.That(game.Combat(0, 1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void TemporaryMurderTest()
+        {
+            var c1 = new Creature(1, 1);
+            var c2 = new Creature(2, 2);
+            CardGame game = new TemporaryCardDamageGame(new[] { c1, c2 });
+            Assert.That(game.Combat(0, 1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void DoubleMurderTest()
+        {
+            var c1 = new Creature(2, 2);
+            var c2 = new Creature(2, 2);
+            CardGame game = new TemporaryCardDamageGame(new[] { c1, c2 });
+            Assert.That(game.Combat(0, 1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void PermanentDamageDeathTest()
+        {
+            var c1 = new Creature(1, 2);
+            var c2 = new Creature(1, 3);
+            CardGame game = new PermanentCardDamage(new[] { c1, c2 });
+            Assert.That(game.Combat(0, 1), Is.EqualTo(-1));
+            Assert.That(game.Combat(0, 1), Is.EqualTo(1));
+        }
+    }
+}
